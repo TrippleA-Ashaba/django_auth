a3a820ce6bebd98e939d0b2bb496bfdcffa960f1
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-12-12 21:44:50 +0300

    sorted and removed redundant imports

--- user/views.py
+++ user/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
